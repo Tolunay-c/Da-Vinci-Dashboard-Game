3f79b3dfea9fdc1df00fe181799682302f7756b7
Tolunay-c <tolunaycagl@hotmail.com>
2025-09-07 00:52:42 +0300

    structure
    Created project structure front-end and back-end

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Da-Vinci-Dashboard-Game-
+# Da-Vinci-Dashboard-Game
 Da Vinci Dashboard Game Case
---
+++ front-end/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+
+
+  return (
+    <>
+     <div className="text-5xl font-bold text-blue-900">geldim ha abe heyran </div>
+    </>
+  )
+}
+
+export default App
